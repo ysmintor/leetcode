cd5a9e49b74bbf6b680f64376079f2865751a4a4
YorkYu <ysmintor@gmail.com>
2019-10-02 12:53:36 +0800

    add 0078.Subsets other two solutions

--- python/0078.subsets/subsets.py
+++ python/0078.subsets/subsets.py
@@ -12,6 +12,39 @@ class Solution:
                 output.append(t1)
         return output
 
+    # based on binary tree method
+    def subsets2(selfs, nums: List[int]) -> List[List[int]]:
+        output = []
+        size = len(nums)
+        def getSubsets(pos: int, out:List[int]):
+            output.append(out)
+            for i in range(pos, size):
+                out.append(nums[i])
+                getSubsets(i+1, out.copy())
+                out.pop()
+        getSubsets(0, [])
+        return output
+
+    # bitwise simulation method
+    def subsets3(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        maxLimit = 1 << len(nums)
+        def int2set(num: int):
+            out = []
+            idx = 0
+            while num > 0:
+                if num & 1 == 1:
+                    out.append(nums[idx])
+                idx += 1
+                num >>= 1
+            return out
+
+        for i in range(maxLimit):
+            out = int2set(i)
+            output.append(out)
+
+        return output
+
 if __name__ == '__main__':
     solution = Solution()
     print(solution.subsets([1,2,3]))
