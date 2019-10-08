b0b4c8d304911c78484095433f21adc7b3302936
YorkYu <ysmintor@gmail.com>
2019-10-08 15:12:17 +0800

    add 0692.Top K Frequent Words

---
+++ python/0692.top-k-frequent-words/top-k-frequent-words.py
@@ -0,0 +1,9 @@
+from heapq import nsmallest
+class Solution:
+    # 与0347 那个题目一样使用堆排序来处理
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        count = collections.Counter(words)
+        heap = [(-freq, word) for word, freq in count.items()]
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for _ in range(k)]
+
